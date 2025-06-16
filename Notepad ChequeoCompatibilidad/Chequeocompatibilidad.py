8d63057e407b6e0c4895cfd76dd2de0b6195d247
agus-domi <agustindomxd@gmail.com>
2025-06-16 12:56:17 -0300

    Comentarios de ChequeoCompatibilidad

--- Notepad ChequeoCompatibilidad/Chequeocompatibilidad.py
+++ Notepad ChequeoCompatibilidad/Chequeocompatibilidad.py
@@ -1,17 +1,21 @@
+#Este programa calcula la compatibilidad entre dos personas basándose en sus nombres.
+#Entrada de Datos
 nombre1 = input("Ingrese el nombre de la primera persona: ")
 nombre2 = input("Ingrese el nombre de la segunda persona: ")
+#Preparación de Letras
 letras = []
 if len(nombre1) > len(nombre2):
     letras = list(nombre1.lower()) + list(nombre2.lower())
 else:
     letras = list(nombre2.lower()) + list(nombre1.lower())
+#Conteo de Letras Unicas
 n = []
 usadas = []
 for letra in letras:
     if letra not in usadas:
         n.append(letras.count(letra))
         usadas.append(letra)
-
+#Función de Compatibilidad(Reducir la lista de números)
 def compatibilidad(numeros):
     mitad = len(numeros) // 2 - 1
     if len(numeros) % 2 == 0:
@@ -29,6 +33,9 @@ def compatibilidad(numeros):
 # 2, 2, 2, 2, 1
 # 3, 4, 2
 # 5, 4
+#Proceso Principal
+# Mientras la lista de números tenga más de 2 elementos, se aplica la función de compatibilidad.
 while len(n) > 2:
     n = compatibilidad(n)
-print(n[0]*10+n[1])
+#Resultado Final
+print(f"La compatibilidad entre {nombre1} y {nombre2} es de {n[0]*10+n[1]}%")
